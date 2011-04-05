69a39c725cb8d303bce1422dd2467df3e1a01d6e
Toni Dietze <Toni.Dietze@tu-dresden.de>
2011-04-05 08:43:31 +0200

    converted tabs into spaces (ghc -fwarn-tabs); removed white spaces at ends of lines

--- Algorithms/NBest.hs
+++ Algorithms/NBest.hs
@@ -66,15 +66,15 @@ instance (Eq a,Ord b) => Ord (Pair a b) where
 data M a = E | M a [M a]
 
 instance Ord a => Eq (M a) where
-	E == E = True
-	M a1 _ == M a2 _ = a1 == a2
-	_ == _ = False
+  E == E = True
+  M a1 _ == M a2 _ = a1 == a2
+  _ == _ = False
 
 instance Ord a => Ord (M a) where
-	compare E E = EQ
-	compare E _ = LT
-	compare _ E = GT
-	compare (M a1 _) (M a2 _) = compare a1 a2
+  compare E E = EQ
+  compare E _ = LT
+  compare _ E = GT
+  compare (M a1 _) (M a2 _) = compare a1 a2
 
 
 lft :: HGraph hNode hEdge hWeight -> HGraph hNode hEdge (Pair (HPath hEdge) hWeight)
@@ -91,37 +91,37 @@ lft  (hNodes,hBack,hWeights) = (hNodes,hBack,hWeights')
 
 knuth :: (Ord hWeight, Ord hNode, Ord hEdge) => HGraph hNode hEdge hWeight -> hNode -> (Maybe (hWeight,hEdge))
 knuth h@(hNodes,_,_)= (Map.!) $ knuth' h ((Map.fromList [(v, Nothing) | v <- hNodes]), (Set.fromList hNodes), Set.empty)
-	
+
 knuth' ::  (Ord hWeight, Ord hNode, Ord hEdge) => HGraph hNode hEdge hWeight -> (Map hNode (Maybe (hWeight,hEdge)), Set hNode, Set hNode) -> Map hNode (Maybe (hWeight,hEdge))
 knuth' h@(_,hBack,hWeights) (oldMap,unvis,vis)
     | Set.null unvis = oldMap
     | otherwise  = knuth' h ((Map.insert v m oldMap), (Set.delete v unvis), (Set.insert v vis))
-	where
-			f sym weights = do b <- (sequence weights)
-			                   let (ws,_) = unzip b
-			                   return ((hWeights sym ws),sym)		
-			list = [(v', (f sym ((map ((Map.!) oldMap) srcs)))) | v' <- Set.toList unvis, (sym,srcs) <- (hBack v'), (Set.isSubsetOf (Set.fromList srcs) vis)]
-			list'= filter (\(_,m') -> isJust m') list
-			(v,m) = minimumBy (\(_,(Just (x,_))) (_,(Just (x',_)))-> compare x x') list'
-			
-			
+  where
+      f sym weights = do b <- (sequence weights)
+                         let (ws,_) = unzip b
+                         return ((hWeights sym ws),sym)
+      list = [(v', (f sym ((map ((Map.!) oldMap) srcs)))) | v' <- Set.toList unvis, (sym,srcs) <- (hBack v'), (Set.isSubsetOf (Set.fromList srcs) vis)]
+      list'= filter (\(_,m') -> isJust m') list
+      (v,m) = minimumBy (\(_,(Just (x,_))) (_,(Just (x',_)))-> compare x x') list'
+
+
 kknuth :: (Ord hWeight, Ord hNode, Ord hEdge) => HGraph hNode hEdge hWeight -> hNode -> (Maybe (hWeight,hEdge))
 kknuth h@(hNodes,_,_)= (Map.!) m
-	where (m,_,_) = kknuth' h hNodes
-	
+  where (m,_,_) = kknuth' h hNodes
+
 kknuth' ::  (Ord hWeight, Ord hNode, Ord hEdge) => HGraph hNode hEdge hWeight -> [hNode] -> (Map hNode (Maybe (hWeight,hEdge)), Set hNode, Set hNode)
 kknuth' (hNodes,_,_) [] = ((Map.fromList [(v, Nothing) | v <- hNodes]), (Set.fromList hNodes), Set.empty)
 kknuth' h@(_,hBack,hWeights) (_:xs)
     | (null list') = (oldMap, unvis, vis)
     | otherwise  = ((Map.insert v m oldMap), (Set.delete v unvis), (Set.insert v vis))
-	where
-			(oldMap,unvis,vis) = kknuth' h xs
-			f sym weights = do b <- (sequence weights)
-			                   let (ws,_) = unzip b
-			                   return ((hWeights sym ws),sym)		
-			list = [(v', (f sym ((map ((Map.!) oldMap) srcs)))) | v' <- Set.toList unvis, (sym,srcs) <- (hBack v'), (Set.isSubsetOf (Set.fromList srcs) vis)]
-			list'= filter (\(_,m') -> isJust m') list
-			(v,m) = minimumBy (\(_,(Just (x,_))) (_,(Just (x',_)))-> compare x x') list'			
+  where
+      (oldMap,unvis,vis) = kknuth' h xs
+      f sym weights = do b <- (sequence weights)
+                         let (ws,_) = unzip b
+                         return ((hWeights sym ws),sym)
+      list = [(v', (f sym ((map ((Map.!) oldMap) srcs)))) | v' <- Set.toList unvis, (sym,srcs) <- (hBack v'), (Set.isSubsetOf (Set.fromList srcs) vis)]
+      list'= filter (\(_,m') -> isJust m') list
+      (v,m) = minimumBy (\(_,(Just (x,_))) (_,(Just (x',_)))-> compare x x') list'
 --------------------------------------------------------------------
 --      Calculation of n-best hyperpaths                          --
 --------------------------------------------------------------------
@@ -135,19 +135,19 @@ q h@(hNodes,hBack,hWeights) kn = (Map.!) m
 
 p :: Ord hWeight => MinHeap (M hWeight) -> [hWeight]
 p heap = case Heap.view heap of
-	Nothing -> []
-	Just (E, heap') -> p heap'
-	Just ((M a l), heap') -> a:(p (Heap.union heap' (Heap.fromList l)))
+  Nothing -> []
+  Just (E, heap') -> p heap'
+  Just ((M a l), heap') -> a:(p (Heap.union heap' (Heap.fromList l)))
 
 topconcat :: Eq hWeight => ([hWeight] -> hWeight) -> [[hWeight]] -> M hWeight
 topconcat f lists
-	| elem [] lists = E
-	| otherwise = M (f (map head lists)) (map (topconcat f) (tail (combine lists)))
+  | elem [] lists = E
+  | otherwise = M (f (map head lists)) (map (topconcat f) (tail (combine lists)))
     where
         combine :: [[a]] -> [[[a]]]
         combine [] = [[]]
         combine (x:xs) = (map (x:) c) ++ [((tail x):(map (\ x' -> [head x']) xs))]
-	        where c = combine xs
+          where c = combine xs
 
 
 mytail :: M a -> [M a]
@@ -181,7 +181,7 @@ main = do
 {-
 knuth :: (Ord hWeight, Ord hNode, Ord hEdge) => HGraph hNode hEdge hWeight -> hNode -> (Maybe (hWeight,hEdge))
 knuth h@(hNodes,hBack,hWeights)= (Map.!) $ knuth' ((Map.fromList [(v, Nothing) | v <- hNodes]), (Set.fromList hNodes), Set.empty)
-	where
+  where
             blaa = concatMap (\x -> map  (\(s,ls) -> (x,s,ls,length ls)) $ hBack x  ) hNodes
             counter1 = Map.fromList $ map (\(_,s,_,l)-> (s,l)) blaa
             counter2 = Map.fromListWith (++) $ map (\(_,s,_,l)-> (l,s)) blaa
@@ -194,17 +194,17 @@ knuth h@(hNodes,hBack,hWeights)= (Map.!) $ knuth' ((Map.fromList [(v, Nothing) |
             knuth'  (oldMap,unvis,vis)
                 | Set.null unvis = oldMap
                 | otherwise  = knuth' ((Map.insert v m oldMap), (Set.delete v unvis), (Set.insert v vis))
-	            where
-			        f sym weights = do b <- (sequence weights)
-			                           let (ws,_) = unzip b
-			                           return ((hWeights sym ws),sym)		
-			        list = [(v', (f sym ((map ((Map.!) oldMap) srcs)))) | v' <- Set.toList unvis, (sym,srcs) <- (hBack v'), (Set.isSubsetOf (Set.fromList srcs) vis)]
-			        list'= filter (\(_,m) -> isJust m) list
-			        (v,m) = minimumBy (\(_,(Just (x,_))) (_,(Just (x',_)))-> compare x x') list'
-			
-			--        locked :: Map hNode -> [hEdge]
-			 --       aux :: Map hNode -> [hEdge]
-			  --      unlocked :: Map hNode -> Set hEdge
+              where
+              f sym weights = do b <- (sequence weights)
+                                 let (ws,_) = unzip b
+                                 return ((hWeights sym ws),sym)
+              list = [(v', (f sym ((map ((Map.!) oldMap) srcs)))) | v' <- Set.toList unvis, (sym,srcs) <- (hBack v'), (Set.isSubsetOf (Set.fromList srcs) vis)]
+              list'= filter (\(_,m) -> isJust m) list
+              (v,m) = minimumBy (\(_,(Just (x,_))) (_,(Just (x',_)))-> compare x x') list'
+
+      --        locked :: Map hNode -> [hEdge]
+       --       aux :: Map hNode -> [hEdge]
+        --      unlocked :: Map hNode -> Set hEdge
 
 
 
@@ -224,9 +224,9 @@ test = do
            --     (Right (a,b)) -> putStr $ showHGraph b
                   (Left err) -> print err
                   (Right err) -> putStrLn (showHGraph $ snd err)
-			
-	
-p :: Vertex -> [BufferedHyperpath] 	
+
+
+p :: Vertex -> [BufferedHyperpath]
 p vertex = a!vertex
   where
   {
@@ -236,36 +236,36 @@ p vertex = a!vertex
       second v = [topconcat symbol' (map p source) | (symbol',source,destination) <- hyperedges, symbol' /= (topsymbol (knuth!v)), v == destination  ];
       topsymbol (Just (Branch symbol _,_)) = symbol
   }
-	
 
 
--- p :: Vertex -> [BufferedHyperpath] 	
+
+-- p :: Vertex -> [BufferedHyperpath]
 -- p vertex
 --   | isNothing mininmalHyperpath = []
---   | otherwise 			= (fromJust mininmalHyperpath):(merge (first ++ second))
+--   | otherwise       = (fromJust mininmalHyperpath):(merge (first ++ second))
 --   where {
 --   first = [tail (topconcat symbol' (map p source) ) | (symbol',source,destination) <- hyperedges, symbol' == symbol, vertex == destination  ];
 --   second = [topconcat symbol' (map p source) | (symbol',source,destination) <- hyperedges, symbol' /= symbol, vertex == destination  ];
--- 				mininmalHyperpath 	= knuth!vertex;
--- 				(Branch symbol _,_)	= fromJust mininmalHyperpath;
--- 	}
+--         mininmalHyperpath   = knuth!vertex;
+--         (Branch symbol _,_)  = fromJust mininmalHyperpath;
+--   }
 -- merges a orderd lists of hyperpath to an ordered list
 -- merge :: [[BufferedHyperpath]] -> [BufferedHyperpath]
 -- merge list
 --  | sortedList == [] = []
---  | otherwise  = (head (head sortedList)):(merge ((tail (head sortedList)):(tail sortedList))) 	
--- 		where 	{	
---		      sortedList = (sortBy (\(x:xs) (y:ys)-> bufferedHyperpathCompare x y) list); -- (filter (\x-> let b = (not.null) x in trace (show b) b) list));
---			}
-			
+--  | otherwise  = (head (head sortedList)):(merge ((tail (head sortedList)):(tail sortedList)))
+--     where   {
+--          sortedList = (sortBy (\(x:xs) (y:ys)-> bufferedHyperpathCompare x y) list); -- (filter (\x-> let b = (not.null) x in trace (show b) b) list));
+--      }
+
 
 merge :: [[BufferedHyperpath]] -> [BufferedHyperpath]
 merge l = merge' $ sortBy (\(x:xs) (y:ys)-> bufferedHyperpathCompare x y) $ filter (not.null) l
-	where
-		merge' :: [[BufferedHyperpath]] -> [BufferedHyperpath]
-		merge' [] = []
-		merge' (x:xs) = head x:binarymerge (tail x) (merge' xs) -- FAST!!!
-		-- merge' (x:xs) = head x:merge ((tail x):xs) -- SLOW!!!
+  where
+    merge' :: [[BufferedHyperpath]] -> [BufferedHyperpath]
+    merge' [] = []
+    merge' (x:xs) = head x:binarymerge (tail x) (merge' xs) -- FAST!!!
+    -- merge' (x:xs) = head x:merge ((tail x):xs) -- SLOW!!!
 
 -- binary merge (merge two sorted lists into one)
 binarymerge :: [BufferedHyperpath] -> [BufferedHyperpath] -> [BufferedHyperpath]
@@ -276,25 +276,25 @@ binarymerge l1@(x:xs) l2@(y:ys)
   | otherwise = x:binarymerge xs l2
   where cmp = bufferedHyperpathCompare x y
 
--- 			
+--
 topconcat :: Symbol -> [[BufferedHyperpath]] -> [BufferedHyperpath]
 topconcat symbol lists
-	| elem [] lists = []
-	| otherwise = (f' symbol (map head lists)):(merge (map (topconcat symbol) (tail (combine lists))))
+  | elem [] lists = []
+  | otherwise = (f' symbol (map head lists)):(merge (map (topconcat symbol) (tail (combine lists))))
 
 
 combine :: [[a]] -> [[[a]]]
 combine [] = [[]]
 combine (x:xs) = (map (x:) c) ++ [((tail x):(map (\x-> [head x]) xs))]
-	where c = combine xs
+  where c = combine xs
 
 -- combine [] = [[]]
 -- combine (x:xs) = (map (([head x]):) c ) ++ (map ((tail x):) c )
--- 	where c = combine xs
-	
+--   where c = combine xs
+
 --------------------------------------------------------------------
 --      Testing                                                   --
---------------------------------------------------------------------	
+--------------------------------------------------------------------
 main = do
   args <- getArgs
   let (n:_) = (map read args)::[Int]
@@ -303,50 +303,50 @@ main = do
   print $ take n $p 0
  -- BS.putStr (myshowlist $ fst $ unzip $ take n $p 0)
 
-	
+
 --------------------------------------------------------------------
 --      EOF                                                       --
 --------------------------------------------------------------------
 -- merge :: [[BufferedHyperpath]] -> [BufferedHyperpath]
 -- merge list
--- 	| filteredlist == [] = []
---  	| otherwise  = m:(merge (first	++ second)) 	
---  		where 	{	
--- 				filteredlist 	= filter (/= []) list; -- remove all empty list
--- 				m    		= minimumBy bufferedHyperpathCompare [head l | l<- filteredlist]; -- select a smallest head of the lists
--- 				first 		= {-# SCC "first" #-}[tail l | l<- filteredlist, (head l) == m]; -- removed m from its corresponding list
--- 				second 		= {-# SCC "second" #-}[l | l<- filteredlist, (head l) /= m];
--- 		}
+--   | filteredlist == [] = []
+--    | otherwise  = m:(merge (first  ++ second))
+--      where   {
+--         filteredlist   = filter (/= []) list; -- remove all empty list
+--         m        = minimumBy bufferedHyperpathCompare [head l | l<- filteredlist]; -- select a smallest head of the lists
+--         first     = {-# SCC "first" #-}[tail l | l<- filteredlist, (head l) == m]; -- removed m from its corresponding list
+--         second     = {-# SCC "second" #-}[l | l<- filteredlist, (head l) /= m];
+--     }
 
-	
-	
-	
---	knuth :: ? -> Array Vertex (Maybe BufferedHyperpath)			
+
+
+
+--  knuth :: ? -> Array Vertex (Maybe BufferedHyperpath)
 -- knuth u q              =  array (minimum vertices,maximum vertices) [(v, knuth' v) | v <- vertices]
 
 -- minimalhyperedge = minimumBy bufferedHyperpathCompare [ f' symbol [] | (symbol, sources, destination) <- hyperedges, sources == [] ]
 
--- knuth' :: Vertex -> Maybe BufferedHyperpath	
+-- knuth' :: Vertex -> Maybe BufferedHyperpath
 -- knuth' v = minimumBy bufferedHyperpathCompare [f' symbol (fromJust (result sources))  | (symbol, sources, destination) <- hyperedges, destination == v, isJust (result sources) ]
--- 		result sources = sequence (map ((!) knuth) sources)
-		
-	
-	
--- 	[(symbol, sources, destination) <- hyperedges]
--- 	minimumBy bufferedHyperpathCompare [f' symbol (fromJust (result sources))  | (symbol, sources, destination) <- hyperedges, destination == v, isJust (result sources) ]
--- 		result sources = sequence (map ((!) knuth) sources)
-	
+--     result sources = sequence (map ((!) knuth) sources)
 
--- 	(array (minimum vertices,maximum vertices) [(v, Nothing) | v <- vertices],[])
-		
+
+
+--   [(symbol, sources, destination) <- hyperedges]
+--   minimumBy bufferedHyperpathCompare [f' symbol (fromJust (result sources))  | (symbol, sources, destination) <- hyperedges, destination == v, isJust (result sources) ]
+--     result sources = sequence (map ((!) knuth) sources)
+
+
+--   (array (minimum vertices,maximum vertices) [(v, Nothing) | v <- vertices],[])
+
 -- bei zugriff auf array an position v: erstmal gucken ob in v die kleinste nullstellige kante reingeht
 
 
 -- instance Eq BufferedHyperpath where
---	(hyperpath,value) == (hyperpath',value') = (value == value') && (hyperpath == hyperpath')
-	
+--  (hyperpath,value) == (hyperpath',value') = (value == value') && (hyperpath == hyperpath')
+
 -- instance Ord BufferedHyperpath where
--- 	(hyperpath,value) <= (hyperpath',value') = (value <= value')
+--   (hyperpath,value) <= (hyperpath',value') = (value <= value')
 
 
 -- ghc -O2 -fglasgow-exts -threaded -prof -auto-all -caf-all -fforce-recomp neu.hs
--- EMTest.hs
+++ EMTest.hs
@@ -13,27 +13,27 @@ foreach :: [i] -> (i -> s -> s) -> s -> s
 foreach l iter = foldr (flip (.)) id (map iter l)
 
 data State n t i = State
-	(S.Set (Int,n,Int))
-	[((Int,n,Int),(Int,n,Int),(Int,n,Int),i)]
+  (S.Set (Int,n,Int))
+  [((Int,n,Int),(Int,n,Int),(Int,n,Int),i)]
 
 fst3 :: (a, b, c) -> a
 fst3 (a,_,_) = a
 
 cyk :: (Ord n,Eq t) => Grammar n t i -> [t] -> Grammar (Int, n, Int) t i
 cyk (l1, l2) w = (reverse l1', l2')
-	where
-	-- l2' is simple
-	l2' = [((i,n,i+1),t,_id) | (i,t) <- zip [0..] w, (n,t',_id) <- l2, t' == t]
-	-- for l1', we use foreach
-	State _ l1' = foreach index iter initial
-	index = [(i,j,k,p) | k <- [2..m], i <- [0..m-k], j <- [1..k-1], p <- l1 ]
-	m = length w
-	initial = State (S.fromList . fst3 . unzip3 $ l2') []
-	iter (i,j,k,(n1,n2,n3,_id)) state@(State items l1'')
-		| (i,n2,i+j) `S.member` items && (i+j,n3,i+k) `S.member` items = state1
-		| otherwise = state
-		where
-			state1 = State (S.insert (i,n1,i+k) items) (((i,n1,i+k),(i,n2,i+j),(i+j,n3,i+k),_id):l1'')
+  where
+  -- l2' is simple
+  l2' = [((i,n,i+1),t,_id) | (i,t) <- zip [0..] w, (n,t',_id) <- l2, t' == t]
+  -- for l1', we use foreach
+  State _ l1' = foreach index iter initial
+  index = [(i,j,k,p) | k <- [2..m], i <- [0..m-k], j <- [1..k-1], p <- l1 ]
+  m = length w
+  initial = State (S.fromList . fst3 . unzip3 $ l2') []
+  iter (i,j,k,(n1,n2,n3,_id)) state@(State items l1'')
+    | (i,n2,i+j) `S.member` items && (i+j,n3,i+k) `S.member` items = state1
+    | otherwise = state
+    where
+      state1 = State (S.insert (i,n1,i+k) items) (((i,n1,i+k),(i,n2,i+j),(i+j,n3,i+k),_id):l1'')
 
 makeHyper
   :: (Ord n)
@@ -66,26 +66,26 @@ doEM g@(l1,l2) n0 es
 
 main :: IO ()
 main = do
-	--print (cyk ([('Z','Z','Z',1)],[('Z','a',2)]) "aaaaa")
-	putStr . prettyIndentBracktes . show $ g
-	-- putStr . prettyIndentBracktes . show $ es
-	putStr . prettyIndentBracktes . show $ zip es $ map (cyk g) es
-	putStr . prettyIndentBracktes . show $ map (mapSnd M.assocs) $ doEM g 'S' es
-	putStr "\n" where
-	  -- g = ([('Z','Z','Z',1)],[('Z','a',2)])
-	  -- g = ([('Z','A','Z',1),('Z','Z','A',2),('Z','A','A',3)],[('A','a',4)])
+  --print (cyk ([('Z','Z','Z',1)],[('Z','a',2)]) "aaaaa")
+  putStr . prettyIndentBracktes . show $ g
+  -- putStr . prettyIndentBracktes . show $ es
+  putStr . prettyIndentBracktes . show $ zip es $ map (cyk g) es
+  putStr . prettyIndentBracktes . show $ map (mapSnd M.assocs) $ doEM g 'S' es
+  putStr "\n" where
+    -- g = ([('Z','Z','Z',1)],[('Z','a',2)])
+    -- g = ([('Z','A','Z',1),('Z','Z','A',2),('Z','A','A',3)],[('A','a',4)])
     g = (
-	    [
-	      ('S','S','S',1)
-	    , ('S','A','T',2)
-	    , ('T','S','B',3)
-	    , ('S','A','B',4)
-	    , ('S','B','A',5)
-	    ],
-	    [
-	      ('S','c',6)
-	    , ('A','a',7)
-	    , ('B','b',8)
-	    ])
+      [
+        ('S','S','S',1)
+      , ('S','A','T',2)
+      , ('T','S','B',3)
+      , ('S','A','B',4)
+      , ('S','B','A',5)
+      ],
+      [
+        ('S','c',6)
+      , ('A','a',7)
+      , ('B','b',8)
+      ])
     es = ["abc", "acb", "abab"]
 
