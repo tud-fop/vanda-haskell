e926a320746a690988106106a9d78ba7847ad287
Toni Dietze <Toni.Dietze@tu-dresden.de>
2011-05-02 11:34:49 +0200

    new: Tools.PrettyPrint.putStrColumns

--- Tools/PrettyPrint.hs
+++ Tools/PrettyPrint.hs
@@ -1,6 +1,12 @@
 -- Copyright (c) 2010, Toni Dietze
 
-module Tools.PrettyPrint(prettyIndentBracktes) where
+module Tools.PrettyPrint
+( prettyIndentBracktes
+, putStrColumns
+) where
+
+
+import Data.List (transpose)
 
 
 indentation :: Int -> String
@@ -19,3 +25,29 @@ prettyIndentBracktes s
             | c `elem` " "   = c : pp cs i l
             | otherwise      = (if l then '\n' : indentation i else []) ++ c : pp cs i False
 
+
+putStrColumns :: [String] -> [[String]] -> IO ()
+putStrColumns seps cols
+  = putStr
+  . unlines
+  . map concat
+  . transpose
+  $ columnalign seps cols
+
+
+columnalign :: [String] -> [[String]] -> [[String]]
+columnalign seps cols
+  = map' (\ (sep, col, l) -> map (\ x -> trim l x ++ sep) col)
+         (\ (_  , col, _) -> col)
+  $ zip3 (cycle seps) cols
+  $ map (maximum . map length) cols
+  where
+    map' _ g [x]      = [g x]
+    map' f g (x : xs) = f x : map' f g xs
+    map' _ _ []       = []
+
+
+trim :: Int -> String -> String
+trim 0 _        = []
+trim n (c : cs) = c : trim (n - 1) cs
+trim n []       = replicate n ' '
