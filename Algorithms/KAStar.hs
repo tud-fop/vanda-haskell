a44450f69e1756d11d8a0447012ff8949143c0ce
Johannes Osterholzer <oholzer@gmx.de>
2011-05-18 23:49:42 +0200

    refactored chart from tuple to record

--- Algorithms/KAStar.hs
+++ Algorithms/KAStar.hs
@@ -35,66 +35,60 @@ import Data.Hypergraph
 --     -- ^ Modified inside derivation, with list of derivation item ranks
 
 
-data Item v l w i = 
-    I { iNode :: v
-      , iEdge :: Hyperedge v l w i
-      , iWeight :: w
-      }
-  | O { oNode :: v
-      , oEdge :: Hyperedge v l w i
-      , oWeight :: w
-      }
-  | K { kNode         :: v
-      , kEdge         :: Hyperedge v l w i
-      , kRank         :: Int
-      , kBackpointers :: [Int]
-      , kWeight :: w
-      } 
-  | D { dNode :: v
-      , dEdge :: Hyperedge v l w i
-      , dBackpointers :: [Int]
-      , dWeight :: w
-      } 
-  deriving Show
+data Item v l w i = Inside (I v l w i)
+                  | Outside (O v l w i)
+                  | Ranked (K v l w i)
+                  | Derivation (D v l w i)
+                  deriving Show
 
+data I v l w i = I { iNode :: v
+                   , iEdge :: Hyperedge v l w i
+                   , iWeight :: w
+                   } deriving Show
+
+data O v l w i = O { oNode :: v
+                   , oEdge :: Hyperedge v l w i
+                   , oWeight :: w
+                   } deriving Show
+
+data K v l w i = K { kNode         :: v
+                   , kEdge         :: Hyperedge v l w i
+                   , kRank         :: Int
+                   , kBackpointers :: [Int]
+                   , kWeight :: w
+                   } deriving Show
+
+data D v l w i = D { dNode :: v
+                   , dEdge :: Hyperedge v l w i
+                   , dBackpointers :: [Int]
+                   , dWeight :: w
+                   } deriving Show
 
 weight :: Item v l w i -> w
-weight (I _ _ w)     = w
-weight (O _ _ w)     = w
-weight (K _ _ _ _ w) = w
-weight (D _ _ _ w)   = w
+weight (Inside (I _ _ w))       = w
+weight (Outside (O _ _ w))      = w
+weight (Ranked (K _ _ _ _ w))   = w
+weight (Derivation (D _ _ _ w)) = w
 
 edge :: Item v l w i -> Hyperedge v l w i
-edge (I _ e _)     = e
-edge (O _ e _)     = e
-edge (K _ e _ _ _) = e
-edge (D _ e _ _)   = e
+edge (Inside (I _ e _))       = e
+edge (Outside (O _ e _))      = e
+edge (Ranked (K _ e _ _ _))   = e
+edge (Derivation (D _ e _ _)) = e
 
 
 -- | Chart of already explored items with their weights.
 --   Implemented as a map assigning nodes to their corresponding inside,
 --   outside, etc., items. Lists are sorted by /decreasing/ weights.
-type Chart v l w i = M.Map v ( [Item v l w i] -- Inside items
-                             , [Item v l w i] -- Outside items
-                             , [Item v l w i] -- Ranked derivation items
-                             , [Item v l w i] -- Modified derivation items
-                             )
+type Chart v l w i = M.Map v (ChartEntry v l w i)
 
-insideItems :: Ord v => Chart v l w i -> v -> [Item v l w i]
-insideItems c v = let (i, _, _, _) = c ! v
-                  in i
-
-outsideItems :: Ord v => Chart v l w i -> v -> [Item v l w i]
-outsideItems c v = let (_, o, _, _) = c ! v
-                  in o
-
-rankedItems :: Ord v => Chart v l w i -> v -> [Item v l w i]
-rankedItems c v = let (_, _, r, _) = c ! v
-                  in r
-
-derivationItems :: Ord v => Chart v l w i -> v -> [Item v l w i]
-derivationItems c v = let (_, _, _, d) = c ! v
-                  in d
+-- | List of inside, outside, etc. items assigned to a node in the chart
+data ChartEntry v l w i = CE 
+  { ceInside     :: [I v l w i] -- Inside items
+  , ceOutside    :: [O v l w i] -- Outside items
+  , ceRanked     :: [K v l w i] -- Ranked derivation items
+  , ceDerivation :: [D v l w i] -- Modified derivation items
+  }
 
 
 type Agenda v l w i = H.MinPrioHeap w (Item v l w i)
@@ -102,16 +96,15 @@ type Agenda v l w i = H.MinPrioHeap w (Item v l w i)
 
 traceBackpointers 
   :: Ord v 
-  => Item v l w i
+  => K v l w i
   -> Hypergraph v l w i
   -> Chart v l w i
   -> T.Tree (Hyperedge v l w i)
-traceBackpointers (K _ e _ bps _) graph chart = T.Node e $ map f $ zip bps [0..]
-  where f (rank, idx)  = let precs = rankedItems chart (eTail e !! idx)
-                         in traceBackpointers (precs !! (length precs - rank)) 
-                                              graph
-                                              chart
-traceBackpointers _ _ _ = error "Only for ranked derivation items"
+traceBackpointers (K _ e _ bps _) graph chart 
+  = T.Node e $ map traceSubtree $ zip bps [0..]
+    where traceSubtree (rank, idx)  
+            = let precs = ceRanked $ chart ! (eTail e !! idx)
+              in traceBackpointers (precs !! (length precs - rank)) graph chart
 
 
 -- | @kbest k g h G@ computes a list of @k@ best derivations of the goal
