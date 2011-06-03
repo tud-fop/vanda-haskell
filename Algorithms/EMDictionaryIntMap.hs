7833ff9393fbf35da2a643d427ab37fd06701e23
Toni Dietze <Toni.Dietze@tu-dresden.de>
2011-06-03 11:34:06 +0200

    added command line action bestSwap

--- Algorithms/EMDictionaryIntMap.hs
+++ Algorithms/EMDictionaryIntMap.hs
@@ -165,7 +165,9 @@ main = do
   args <- getArgs
   case args of
     ["best", delta, corpus]
-      -> mainTrain (read delta) corpus
+      -> mainTrain False (read delta) corpus
+    ["bestSwap", delta, corpus]
+      -> mainTrain True (read delta) corpus
     ["csv", delta, corpus]
       -> mainSteps (read delta) corpus
     ["generate-test-corpus", wordCnt, wordCntPerSentence]
@@ -179,8 +181,8 @@ main = do
           putStrLn "  csv <maximal delta> <corpus file>"
 
 
-mainTrain :: Double -> String -> IO ()
-mainTrain delta corpus
+mainTrain :: Bool -> Double -> String -> IO ()
+mainTrain swap delta corpus
   = parseCorpus corpus
   >>= putStrColumns [" | "]
     . (\ (x, y, z) -> ["e" : x, "f" : y, "p(f|e)" : z])
@@ -188,6 +190,7 @@ mainTrain delta corpus
     . fmap (\ ((e, f), w) -> (e, f, show w))
     . filter ((<) 0.1 . snd)
     . train delta
+    . (if swap then map (\ (a, b) -> (b, a)) else id)
 
 
 mainSteps :: Double -> String -> IO ()
