d8ff148939754210d90ae309c9e69ef4bac06d17
Toni Dietze <Toni.Dietze@tu-dresden.de>
2011-08-19 16:12:36 +0200

    Tools.Timestamps: new

---
+++ Tools/Timestamps.hs
@@ -0,0 +1,34 @@
+module Tools.Timestamps
+( printTimestamp
+) where
+
+
+import CPUTime
+import Data.Time.Clock
+import Data.Time.LocalTime ()
+import Text.Printf
+
+
+printTimestamp :: IO ()
+printTimestamp = do
+  cpuTime <- getCPUTime
+  wallclockTime <- getCurrentTime
+  printf format
+    (1e-12 * fromIntegral cpuTime :: Double)
+    (show wallclockTime)
+
+
+format :: String
+format = "[%" ++ show (8 + precision) ++ "." ++ show precision ++ "f | %s] "
+
+
+precision :: Integer
+precision = let p = 12 - logI cpuTimePrecision in if p < 0 then 0 else p
+
+
+logI :: Integer -> Integer
+logI = go 0
+  where
+    go e n
+      | n <= 1    = e
+      | otherwise = go (e + 1) (div n 10)
