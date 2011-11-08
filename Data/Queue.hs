ec6a775c38d3105e78dbfa29f67a77eaa12a3eaf
Toni Dietze <Toni.Dietze@tu-dresden.de>
2011-11-08 15:08:04 +0100

    Data/Queue.hs: added fromList; improved show; ++haddock

--- Data/Queue.hs
+++ Data/Queue.hs
@@ -24,6 +24,7 @@ module Data.Queue (
 , deq
 , enqList
 , enqListWith
+, fromList
 , toList
 ) where
 
@@ -31,39 +32,70 @@ module Data.Queue (
 import Prelude hiding (null)
 
 
-data Queue a = Queue [a] [a] deriving (Show)
+data Queue a = Queue [a] [a]
 
 
+instance Eq a => Eq (Queue a) where
+  q1 == q2 = toList q1 == toList q2
+
+
+-- copied from Data.Set
+instance Show a => Show (Queue a) where
+  showsPrec p xs = showParen (p > 10) $
+    showString "fromList " . shows (toList xs)
+
+
+-- | @True@ iff 'Queue' is empty.
 null :: Queue a -> Bool
 null (Queue [] []) = True
 null _             = False
 
 
+-- | The empty 'Queue'.
 empty :: Queue a
 empty = Queue [] []
 
 
+-- | Returns the 'Queue' containing exactly the given element.
 singleton :: a -> Queue a
 singleton x = Queue [x] []
 
 
+-- | Add an element to a 'Queue'.
 enq :: a -> Queue a -> Queue a
 enq y (Queue xs ys) = Queue xs (y:ys)
 
 
+-- | Remove the oldest element from the 'Queue' and return the element and
+-- the 'Queue' without the element.
 deq :: Queue a -> (a, Queue a)
 deq (Queue (x:xs) ys      ) = (x, Queue xs ys)
 deq (Queue []     ys@(_:_)) = deq (Queue (reverse ys) [])
 deq (Queue []     []      ) = error "Cannot dequeue from empty queue."
 
 
+-- | Add all elements of the given list to the 'Queue'.
+-- /The order in which the list elements are added is not defined./
 enqList :: [a] -> Queue a -> Queue a
-enqList zs (Queue xs ys) = Queue xs (foldr (:) ys zs)
+enqList zs (Queue xs ys) = Queue xs (zs ++ ys)
 
 
+-- | Add all elements of the given list to the 'Queue' after applying a
+-- function to them.
+-- /The order in which the elements are added is not defined./
 enqListWith :: (b -> a) -> [b] -> Queue a -> Queue a
 enqListWith f zs (Queue xs ys) = Queue xs (foldr ((:) . f) ys zs)
 
 
+-- | Convert a list to a 'Queue', such that
+-- @deq (fromList (x : xs)) == (x, fromList xs)@.
+-- Additionally, @toList (fromList xs) == xs@.
+fromList :: [a] -> Queue a
+fromList xs = Queue xs []
+
+
+-- | Convert a 'Queue' to a List, such that
+-- @toList q == x : xs@ iff @deq q == (x, fromList xs)@.
+-- Additionally, @fromList (toList q) == q@.
 toList :: Queue a -> [a]
-toList (Queue xs ys) = xs ++ (reverse ys)
+toList (Queue xs ys) = xs ++ reverse ys
