2cd2cf702d56b1de7616115dfa68d24b4c7f4592
Toni Dietze <Toni.Dietze@tu-dresden.de>
2012-08-28 08:43:50 +0200

    src/Tools/Timestamps.hs: added functions putStrLnTimestamped and putStrLnTimestamped'

--- src/Tools/Timestamps.hs
+++ src/Tools/Timestamps.hs
@@ -1,12 +1,16 @@
 module Tools.Timestamps
 ( printTimestamp
+, putStrLnTimestamped
+, putStrLnTimestamped'
 ) where
 
 
+import Control.DeepSeq
 import Data.Time.Clock
 import Data.Time.LocalTime ()
 import System.CPUTime
 import Text.Printf
+
 
 
 printTimestamp :: IO ()
@@ -16,6 +20,14 @@ printTimestamp = do
   printf format
     (1e-12 * fromIntegral cpuTime :: Double)
     (show wallclockTime)
+
+
+putStrLnTimestamped :: String -> IO ()
+putStrLnTimestamped cs = printTimestamp >> putStrLn cs
+
+
+putStrLnTimestamped' :: String -> IO ()
+putStrLnTimestamped' cs = cs `deepseq` printTimestamp >> putStrLn cs
 
 
 format :: String
