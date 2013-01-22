94f6654343a3bfc6ba78cccaff66dffae8918f13
Tobias Denkinger <s7480817@mail.zih.tu-dresden.de>
2013-01-22 15:35:22 +0100

    made functions in Vanda.Grammar.NGrams.KenLM pure, extended argument parsing in programs/NGrams_KenLM.hs

--- programs/NGrams_KenLM.hs
+++ programs/NGrams_KenLM.hs
@@ -14,12 +14,44 @@ main = do
   args <- getArgs
   case args of
     ["-g", grammar, text] -> do
-      nGrams <- loadNGrams grammar
       input  <- TIO.readFile text
-      wts <- sequence
-           . L.map (evaluateLine nGrams)
-           . T.lines
-           $ input
+      let nGrams = loadNGrams grammar
+      let wts = L.map (evaluateLine nGrams)
+              . T.lines
+              $ input
       TIO.putStr . T.unlines
                  . map (T.pack . show)
                  $ wts
+    ["-g", grammar, "-i", text] -> do
+      input  <- TIO.readFile text
+      let nGrams = loadNGrams grammar
+      let wts = L.map (evaluateLine nGrams)
+              . T.lines
+              $ input
+      TIO.putStr . T.unlines
+                 . map (T.pack . show)
+                 $ wts
+    ["-g", grammar, text, "-o", outFile] -> do
+      input  <- TIO.readFile text
+      let nGrams = loadNGrams grammar
+      let wts = L.map (evaluateLine nGrams)
+              . T.lines
+              $ input
+      TIO.writeFile outFile
+                  . T.unlines
+                  . map (T.pack . show)
+                  $ wts
+    ["-g", grammar, "-i", text, "-o", outFile] -> do
+      input  <- TIO.readFile text
+      let nGrams = loadNGrams grammar
+      let wts = L.map (evaluateLine nGrams)
+              . T.lines
+              $ input
+      TIO.writeFile outFile
+                  . T.unlines
+                  . map (T.pack . show)
+                  $ wts
+    _ -> do
+      TIO.putStr
+      . T.pack
+      $ "usage: NGrams_KenLM -g <grammar> [-i] <inputFile> [-o <outputFile>]\n"
--- src/Vanda/Grammar/NGrams/KenLM.hs
+++ src/Vanda/Grammar/NGrams/KenLM.hs
@@ -11,36 +11,88 @@
 -- Stability   :  unknown
 -- Portability :  portable
 --
+-- Uses the C library KenLM from <http://kheafield.com/code/kenlm/> to load
+-- APRA files and score sentences.
+--
 -----------------------------------------------------------------------------
 
 {-# LANGUAGE ForeignFunctionInterface #-}
 
 module Vanda.Grammar.NGrams.KenLM
-  ( loadNGrams
+  ( -- * Loading models
+    loadNGrams
+    -- * Constants
+  , beginSentenceState
+  , nullContextState
+    -- * Scoring
+  , evaluate
   , evaluateLine
   ) where
 
 import Data.Text.Lazy as T
-import Data.List as L
 import Foreign.C
-import Foreign.C.String
 import Foreign.Ptr
-import Control.Monad
+import System.IO.Unsafe
 
 data KenTrieModel = KenTrieModel
+data State = State
 
-foreign import ccall "loadModel" cLoadNGrams :: CString -> IO (Ptr KenTrieModel)
-foreign import ccall "score" cEvaluateLine :: (Ptr KenTrieModel) -> CString -> IO CFloat
+foreign import ccall "loadModel" cLoadNGrams
+                 :: CString -> IO (Ptr KenTrieModel)
 
+foreign import ccall "beginSentenceState" cBeginSentenceState
+                 :: Ptr KenTrieModel -> Ptr State
+
+foreign import ccall "nullContextState" cNullContextState
+                 :: Ptr KenTrieModel -> Ptr State
+
+foreign import ccall "lookup" cLookup
+                 :: Ptr KenTrieModel -> Ptr State -> CString -> IO CFloat
+
+foreign import ccall "score" cEvaluateLine
+                 :: Ptr KenTrieModel -> CString -> IO CFloat
+
+-- | Loads a KenTrieModel from a binary ARPA file containing a TrieModel OR
+-- a textual ARPA file.
 loadNGrams
-  :: FilePath
-  -> IO (Ptr KenTrieModel)
+  :: FilePath                  -- ^ file name
+  -> Ptr KenTrieModel          -- ^ model
 loadNGrams s
-  = withCString s cLoadNGrams
+  = unsafePerformIO $ withCString s cLoadNGrams
 
+-- | Returns the State to use when at the beginning of a sentence.
+beginSentenceState
+  :: Ptr KenTrieModel          -- ^ model
+  -> Ptr State                 -- ^ 'State' for the beginning of a sentence
+beginSentenceState
+  = cBeginSentenceState
+
+-- | Returns the State to use when there is no context.
+nullContextState
+  :: Ptr KenTrieModel          -- ^ model
+  -> Ptr State                 -- ^ 'State' for empty context
+nullContextState
+  = cNullContextState
+
+-- | Scores a phrase beginning with a 'State'.
+evaluate
+  :: Ptr KenTrieModel          -- ^ model
+  -> Ptr State                 -- ^ 'State' to start with
+  -> T.Text                    -- ^ phrase to score
+  -> Float                     -- ^ score
+evaluate m s t
+  = unsafePerformIO
+  $ withCString
+      (T.unpack t)
+      (fmap realToFrac . cLookup m s)
+
+-- | Scores a whole sentence.
 evaluateLine
-  :: (Ptr KenTrieModel)
-  -> T.Text
-  -> IO Float
+  :: Ptr KenTrieModel          -- ^ model
+  -> T.Text                    -- ^ sentence to score
+  -> Float                     -- ^ score
 evaluateLine m s
-  = withCString (T.unpack s) (fmap realToFrac . cEvaluateLine m)
+  = unsafePerformIO
+  $ withCString
+      (T.unpack s)
+      (fmap realToFrac . cEvaluateLine m)
