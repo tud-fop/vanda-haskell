59e905cf1a2f79f877e9b31f1ae9998f87e5f58f
Tobias Denkinger <s7480817@mail.zih.tu-dresden.de>
2013-02-20 16:40:56 +0100

    added program for translation vs. language model intersection

---
+++ programs/XRSNGrams.hs
@@ -0,0 +1,29 @@
+module Main where
+
+import System.Environment ( getArgs )
+import qualified Data.Text.Lazy.IO as TIO
+import qualified Data.Text.Lazy as T
+import qualified Data.Vector as V
+import qualified Data.Vector.Unboxed as VU
+
+import qualified Vanda.Grammar.XRS.Functions as IF
+import qualified Vanda.Grammar.XRS.IRTG as I
+import qualified Vanda.Grammar.NGrams.KenLM as LM
+import qualified Vanda.Algorithms.IntersectWithNGram as IS
+
+main
+  :: IO ()
+main = do
+  args <- getArgs
+  case args of
+    ["-f", fMapFile, "-z", zhgFile, "-l", lmFile] -> do
+      irtg1 <- IF.loadIRTG (zhgFile ++ ".bhg.gz")
+      ws    <- IF.loadWeights (zhgFile ++ ".weights.gz")
+      fm    <- IF.loadTokenArray fMapFile
+      lm    <- LM.loadNGrams lmFile
+      let xrs  = I.XRS irtg1 (VU.generate (V.length ws) (ws V.!))
+      let xrs' = IS.intersect lm . IS.relabel lm fm $ xrs
+      TIO.putStr . T.pack . show . I.h2 . I.irtg $ xrs
+      TIO.putStr . T.pack $ "\n"
+      TIO.putStr . T.pack . show . I.h2 . I.irtg $ xrs'
+      TIO.putStr . T.pack $ "\n"
