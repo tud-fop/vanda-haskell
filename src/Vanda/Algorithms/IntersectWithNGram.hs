a8f08a5497f039117ff793a75a26eab4933bd1fe
Tobias Denkinger <tobias.denkinger@tu-dresden.de>
2013-08-16 11:02:24 +0200

    more performance tuning

--- src/Vanda/Algorithms/IntersectWithNGram.hs
+++ src/Vanda/Algorithms/IntersectWithNGram.hs
@@ -19,7 +19,6 @@
 module Vanda.Algorithms.IntersectWithNGram where
 
 import qualified Data.Map as M
-import qualified Data.List as L
 import qualified Data.Set as S
 import Data.NTT
 import Vanda.Grammar.LM
@@ -38,34 +37,34 @@ intersect
 intersect lm mu h2 hg
   = let es  = filter ((/=) 0 . HI.arity) . HI.edges $ hg
         es0 = filter ((==) 0 . HI.arity) . HI.edges $ hg
-        is0 = M.fromListWith (++)
-            . map (\x -> (HI.to x, [initRule mu h2 lm x]))
-            $ es0
-        go ts nits its
+        is0 = flip map es0 $ initRule mu h2 lm
+        ns0 = M.map S.fromList . M.fromListWith (++) . map (\x -> (_fst x, [x])) . map _to $ is0
+        go cs os ns its
           = let is = [ (HI.to e, lst)
-                     | e  <- es
+                     | e <- es
                      , let lst = [ blowRule mu h2 lm e s
-                                 | s <- L.nub 
-                                      . fst
+                                 | s <- fst
                                       . awesomeSequence
                                       $ [ (nLst, oLst)
                                         | q <- HI.from e
-                                        , let nLst = L.nub . map _to $ M.findWithDefault [] q nits
-                                        , let oLst = L.nub . map _to $ M.findWithDefault [] q its
+                                        , let nLst = S.toList $ M.findWithDefault S.empty q ns
+                                        , let oLst = S.toList $ M.findWithDefault S.empty q os
                                         ]
-                                 , S.notMember s ts
+                                 , S.notMember s cs
                                  ]
                      , not $ null lst
                      ]
-                mNew = M.fromListWith (++) is
+                cs' = flip S.union cs
+                    . S.fromList . map _from
+                    $ concatMap snd is
+                os' = M.unionWith S.union os ns
+                ns' = M.mapWithKey (\ k x -> S.difference x $ M.findWithDefault S.empty k os')
+                    . M.map (S.fromList . map _to)
+                    $ M.fromListWith (++) is
             in  if   null is
-                then concat . map snd
-                            . M.toAscList
-                            $ M.unionWith (++) its nits
-                else go (S.union (S.fromList . map _from . concatMap snd $ is) ts)
-                     mNew
-                   $ M.unionWith (++) its nits
-    in  go S.empty is0 M.empty
+                then its
+                else go cs' os' ns' $ concatMap snd is ++ its
+    in  go S.empty M.empty ns0 is0
 
 -- | Emits an initial 'Item'.
 initRule
@@ -80,11 +79,10 @@ initRule mu h2 lm he
         h (NT x) = x
         st = deltaS lm [] . map h . h2 $ he
         w1 = deltaW lm [] . map h . h2 $ he
-    in  Item 
-          (CState (HI.to he) st)
-          (w1 + (mu he))
-          []
-          (HI.label he)
+    in  Item (CState (HI.to he) st)
+             (w1 + (mu he))
+             []
+             (HI.label he)
 
 -- | Combines 'Item's by a rule. The 'Item's and the rule must
 --   match (not checked).
@@ -97,11 +95,16 @@ blowRule
   -> [CState Int]                   -- ^ base states
   -> Item (CState Int) l Double     -- ^ resulting 'Item'
 blowRule mu h2 lm he xs
-  = let xr      = doReordering (h2 he) . map _snd $ xs
+  = let xr      = doReordering (h2 he) $ map _snd xs
         x       = deltaS lm xr []
         w1      = deltaW lm xr []
-    in  Item (CState (HI.to he) x) (mu he + w1) xs (HI.label he)
+    in  Item (CState (HI.to he) x)
+             (mu he + w1)
+             xs
+             (HI.label he)
 
+-- | Calculates sequences as shown in the following example
+--   awesomeSequence ()
 awesomeSequence :: [([a], [a])] -> ([[a]], [[a]])
 awesomeSequence [] = ([], [[]])
 awesomeSequence ((ns, os) : xs)
