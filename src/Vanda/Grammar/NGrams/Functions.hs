10fa87d5324bec79b197eb180e9142cc67e42c58
Tobias Denkinger <tobias.denkinger@tu-dresden.de>
2013-08-26 13:52:31 +0200

    introduced function for training n-gram models using Backoff, not yet working properly

--- src/Vanda/Grammar/NGrams/Functions.hs
+++ src/Vanda/Grammar/NGrams/Functions.hs
@@ -27,6 +27,8 @@ import qualified Data.Text.IO as TIO
 import Vanda.Grammar.NGrams.VandaNGrams
 import Vanda.Grammar.NGrams.Text
 
+import Debug.Trace
+
 -- | Loads an NGram language model from a file.
 loadNGrams
   :: FilePath                -- ^ file to load the model from
@@ -34,6 +36,46 @@ loadNGrams
 loadNGrams
   = fmap parseNGrams
   . TIO.readFile
+
+-- | completely broken (stupid alphas)
+trainModel
+  :: Int                     -- ^ minimum reliable count (/k/)
+  -> Int                     -- ^ degree of the model (/n/)
+  -> T.Text                  -- ^ text
+  -> NGrams T.Text           -- ^ /n/-gram model
+trainModel k n text
+  = let sentenceCorpus   = M.fromListWith (+) [ (T.words x, 1) | x <- T.lines text ]
+        nGramCorpus      = traceShow' $ M.fromList [ (n', fromSentenceCorpus sentenceCorpus $ fromIntegral n') | n' <- [1 .. n] ]
+        smoothedCorpus   = traceShow' $ M.fromList [ (n', smoothCorpus (fromIntegral k) $ nGramCorpus M.! n') | n' <- [1 .. n] ]
+        nGramWeight      = traceShow' $ M.fromList
+                         $ [ (n', applyRFE (smoothedCorpus M.! n') $ nGramCorpus M.! (n' - 1))
+                           | n' <- [2 .. n]
+                           ]
+                           ++
+                           [ (1, flip M.map (smoothedCorpus M.! 1)
+                               $ (\x -> (x + 1) / (fromIntegral $ M.foldl (+) 0 (nGramCorpus M.! 1)))
+                             )
+                           ]
+        remainingMass    = M.fromList
+                         $ [ (n', calculateRemainingMass (nGramWeight M.! (n' + 1)) (nGramWeight M.! n'))
+                           | n' <- [1 .. (n - 1)]
+                           ]
+                           ++
+                           [ (n, M.empty) ]
+        f m1 m2          = M.unionWith
+                             (\ (x, _) (_, y) -> (x, y))
+                             m1
+                             m2
+        combinedMap      = M.unionWith
+                             f
+                             (M.map (M.map (\ x -> (x, Nothing))) nGramWeight)
+                             (M.map (M.map (\ y -> (0, Just y))) remainingMass)
+        weightedNGrams   = M.assocs . M.unions $ M.elems combinedMap
+        g lm (w, (x, y)) = addNGram lm w x y
+    in  L.foldl' g (empty n) weightedNGrams
+
+
+
 
 -- | Evaluates a sentence given a NGrams model.
 evaluateLine
@@ -51,6 +93,65 @@ evaluateLine lm l
     )
   . T.words
   $ l
+
+-- | Converts a sentence corpus into a /n/-gram corpus.
+fromSentenceCorpus
+  :: Ord v
+  => M.Map [v] Int -- ^ sentence corpus
+  -> Int           -- ^ /n/
+  -> M.Map [v] Int -- ^ /n/-gram corpus
+fromSentenceCorpus m n
+  = M.fromListWith (+)
+  $ [ (gram, m M.! sent)
+    | sent <- M.keys m
+    , gram <- subsequences' n sent
+    ]
+
+-- | Applies Smoothting to a corpus.
+smoothCorpus
+  :: Ord v
+  => Int              -- ^ minimum reliable count (/k/)
+  -> M.Map [v] Int    -- ^ unsmoothed corpus
+  -> M.Map [v] Double -- ^ smoothed corpus
+smoothCorpus k m
+  = M.map f m where
+      f :: Int -> Double
+      f d = if   d > k
+            then fromIntegral d
+            else {-((dd + 1) * (funcN $ d + 1) / (funcN d) - (dd) * (kk + 1) * (funcN $ k + 1) / (funcN 1))
+                      / (1 - (kk + 1) * (funcN $ k + 1) / (funcN 1)) where
+                      kk = fromIntegral k
+                      dd = fromIntegral d -} -- mathematical bullshit
+                 fromIntegral (d + 1) * (funcN $ d + 1) / (funcN d) -- only a workaround
+      funcN :: Int -> Double -- N_c, but add-one smoothing is probably not so smart here
+      funcN d' = fromIntegral $ length [ k' | k' <- M.keys m, m M.! k' == d' ] + 1
+
+-- | Applies RFE with smoothing.
+applyRFE
+  :: Ord v
+  => M.Map [v] Double -- ^ n-gram corpus for numerator, e.g., smoothed corpus
+  -> M.Map [v] Int    -- ^ (/n/-1)-gram corpus for denominator, e.g., unsmoothed corpus
+  -> M.Map [v] Double -- ^ RFE
+applyRFE m1 m2
+  = M.mapWithKey f m1 where
+      f k d = (/) d
+            $ if   length k > 1
+              then fromIntegral . (M.!) m2 . flip take k $ length k - 1
+              else fromIntegral $ sum [ m2 M.! k' | k' <- M.keys m2 ]
+
+-- | Calculates the remaining probability mass.
+calculateRemainingMass
+  :: Ord v
+  => M.Map [v] Double -- ^ (/n/+1)-gram weight function for numerator
+  -> M.Map [v] Double -- ^ /n/-gram weight function for denominator
+  -> M.Map [v] Double -- ^ remaining mass for /n/-grams
+calculateRemainingMass m1 m2
+  = M.fromList
+  $ [ (k, (1 - s1) / (1 - s2))
+    | k <- M.keys m2
+    , let s1 = sum [ m1 M.! k' | k' <- M.keys m1, (reverse . drop 1 $ reverse k') == k ]
+    , let s2 = sum [ m2 M.! k' | k' <- M.keys m2, (reverse . drop 1 $ reverse k') == drop 1 k ]
+    ]
 
 writeNGrams
   :: NGrams T.Text
@@ -82,3 +183,12 @@ writeNGrams lm
                                ]
                    ]
     in  T.unlines $ header grams ++ body grams ++ [T.pack "\\end\\"]
+
+subsequences' :: Int -> [v] -> [[v]]
+subsequences' i xs
+  = if   i <= length xs
+    then (take i xs):(subsequences' i $ drop 1 xs)
+    else []
+
+traceShow' x = trace (f x) x
+f m = unlines . map (\ (x, y) -> show (T.unwords x) ++ " -> " ++ show y) . M.toAscList . M.unions $ M.elems m
