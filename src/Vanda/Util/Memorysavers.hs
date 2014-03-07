4103d4b841b13fedfd1798eb38b79289fdfdd1c4
Toni Dietze <Toni.Dietze@tu-dresden.de>
2014-03-07 10:17:38 +0100

    src/Vanda/Util/Memorysavers.hs: improved API

--- src/Vanda/Util/Memorysavers.hs
+++ src/Vanda/Util/Memorysavers.hs
@@ -20,8 +20,16 @@ module Vanda.Util.Memorysavers
 
   -- ** Intify
   -- | Replace the values in a 'Traversable' data structure by 'Int's in an
-  -- injective manner. The (partial) injection is returned in a 'M.Map'.
-   intify1, intify2, intify3
+  -- injective manner. The (partial) injection is returned in a 'M.Map' and
+  -- its inverse is returned in an 'A.Array'.
+  --
+  -- The primed variants ensure that the resulting data structure is evaluated
+  -- before the 'M.Map' and the 'A.Array' to prevent stack overflows. If your
+  -- code ensures this anyway, you may prefer the slightly faster unprimed
+  -- variants.
+  intify1, intify1'
+, intify2, intify2'
+, intify3, intify3'
 
 , -- ** Unify
   -- | Replace equal values (with respect to 'Eq') in a 'Traversable' data
@@ -30,31 +38,69 @@ module Vanda.Util.Memorysavers
   unify1, unify2, unify3
 
 , -- * Plumbing
-  intifyS0, intifyS1, intifyS2, intifyS3
+  -- $plumbing
+  invertMap
+, intifyS0, intifyS1, intifyS2, intifyS3
 , unifyS0, unifyS1, unifyS2, unifyS3
 ) where
 
 import Prelude hiding (mapM)
 
 import Control.Monad.State.Lazy hiding (mapM)
+import Control.Parallel.Strategies
+import qualified Data.Array as A
 import qualified Data.Map.Lazy as M
 import Data.Traversable (Traversable, mapM)
+import Data.Tuple (swap)
 
 
-intify1 :: (Ord k, Traversable t1) => t1 k -> (t1 Int, M.Map k Int)
-intify1 x = runState (intifyS1 x) M.empty
+intify1
+  :: (Ord e, Traversable t1)
+  =>  t1 e
+  -> (t1 Int, (M.Map e Int, A.Array Int e))
+intify1 x = doIntify intifyS1 x
+
+
+intify1'
+  :: (Ord e, Traversable t1)
+  =>  t1 e
+  -> (t1 Int, (M.Map e Int, A.Array Int e))
+intify1'
+  = withStrategy (evalTuple2 (evalTraversable rseq) r0)
+  . intify1
+
+
+intify2'
+  :: (Ord e, Traversable t1, Traversable t2)
+  =>  t2 (t1 e)
+  -> (t2 (t1 Int), (M.Map e Int, A.Array Int e))
+intify2'
+  = withStrategy (evalTuple2 (evalTraversable (evalTraversable rseq)) r0)
+  . intify2
 
 
 intify2
-  :: (Ord k, Traversable t1, Traversable t2)
-  => t2 (t1 k) -> (t2 (t1 Int), M.Map k Int)
-intify2 x = runState (intifyS2 x) M.empty
+  :: (Ord e, Traversable t1, Traversable t2)
+  =>  t2 (t1 e)
+  -> (t2 (t1 Int), (M.Map e Int, A.Array Int e))
+intify2 x = doIntify intifyS2 x
 
 
 intify3
-  :: (Ord k, Traversable t1, Traversable t2, Traversable t3)
-  => t3 (t2 (t1 k)) -> (t3 (t2 (t1 Int)), M.Map k Int)
-intify3 x = runState (intifyS3 x) M.empty
+  :: (Ord e, Traversable t1, Traversable t2, Traversable t3)
+  =>  t3 (t2 (t1 e))
+  -> (t3 (t2 (t1 Int)), (M.Map e Int, A.Array Int e))
+intify3 x = doIntify intifyS3 x
+
+
+intify3'
+  :: (Ord e, Traversable t1, Traversable t2, Traversable t3)
+  =>  t3 (t2 (t1 e))
+  -> (t3 (t2 (t1 Int)), (M.Map e Int, A.Array Int e))
+intify3'
+  = withStrategy
+    (evalTuple2 (evalTraversable (evalTraversable (evalTraversable rseq))) r0)
+  . intify3
 
 
 unify1 :: (Ord k, Traversable t1) => t1 k -> t1 k
@@ -71,11 +117,42 @@ unify3
 unify3 x = evalState (unifyS3 x) M.empty
 
 
+doIntify
+  :: (s -> State (M.Map e Int) t)
+  -> s
+  -> (t, (M.Map e Int, A.Array Int e))
+doIntify intifyS
+  = \ x -> let (y, m) = runState (intifyS x) M.empty
+            in (y, (m, invertMap m))
+
+
+-- $plumbing
+-- We will use the following terms:
+--
+-- - injectiveness: A 'M.Map' is injective, if it maps different keys to
+--   different values.
+--
+-- - size-boundedness: A 'M.Map' is size-bounded, if all its 'M.elems' are
+--   smaller than the 'M.size' of the 'M.Map'.
+--
+-- - inverve-array-likeness: A 'M.Map' is inverve-array-like, if 'M.elems' of
+--   the 'M.Map' contains every 'Int' from 0 to @('M.size' - 1)@ exactly once.
+--
+-- Note that inverse-array-likeness implies size-boundedness and
+-- injectiveness.
+
+
+-- | Invert an inverse-array-like 'M.Map'.
+-- /Inverse-array-likeness is not checked!/
+invertMap :: M.Map k Int -> A.Array Int k
+invertMap m = A.array (0, M.size m - 1) $ map swap $ M.assocs m
+
+
 -- | The actual worker for the @intify@ functions. Updates the 'M.Map' as
--- needed. The initial 'M.Map' must be a partial injection (e.g. 'M.empty'),
--- and all 'M.elems' of the 'M.Map' must be smaller than the 'M.size' of the
--- 'M.Map'. These conditions are preserved by this function, but they are
--- /not/ checked for the initial 'M.Map'.
+-- needed. The initial 'M.Map' must be a injective and size-bounded.
+-- /This is not checked!/ Inverve-array-likeness is not needed, yet,
+-- 'intifyS0' preserves injectiveness, size-boundedness and
+-- inverse-array-likeness of the 'M.Map'.
 intifyS0 :: Ord k => k -> State (M.Map k Int) Int
 intifyS0 k = do
   m <- get
