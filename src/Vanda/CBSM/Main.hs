e14d3db1be7c8eba8aa9bbdc3aae67c1416229e1
Toni Dietze <toni.dietze@tu-dresden.de>
2014-10-10 09:47:33 +0200

    Vanda.CBSM.Main: switched Vanda.Corpus.{Penn -> SExpression}; improved command line interface

--- src/Vanda/CBSM/Main.hs
+++ src/Vanda/CBSM/Main.hs
@@ -21,7 +21,7 @@ import qualified Data.RevMap as RM
 import           Vanda.Algorithms.EarleyMonadic
 import qualified Vanda.Algorithms.Earley.WSA as WSA
 import           Vanda.CBSM.CountBasedStateMerging
-import           Vanda.Corpus.Penn.Simple as Penn
+import           Vanda.Corpus.SExpression as SExp
 import qualified Vanda.Features as F
 import qualified Vanda.Hypergraph as H
 import           Vanda.Util.Tree as T
@@ -33,62 +33,131 @@ import qualified Data.Binary as B
 import qualified Data.Map as M
 import           Data.Ord
 import qualified Data.Set as S
-import qualified Data.Text.Lazy as Text
-import qualified Data.Text.Lazy.IO as Text
 import           Data.Tree
 import qualified Data.Vector as V
-import           System.Console.CmdArgs
-import           Text.Parsec.String (parseFromFile)
+import           System.Console.CmdArgs.Explicit
+import           System.Console.CmdArgs.Text
+import           System.Directory (doesDirectoryExist, getDirectoryContents)
+import           System.FilePath ((</>))
+
+
+moduleName :: String
+moduleName = "Vanda.CBSM.Main"
+
+
+errorHere :: String -> String -> a
+errorHere fun msg = error $ moduleName ++ "." ++ fun ++ ": " ++ msg
 
 
 data Args
-  = CBSM
-    { argIterations :: Int
-    , argCorpus :: FilePath
-    , argDefoliate :: Bool
-    , argGrammar :: FilePath
+  = Help HelpFormat TextFormat
+  | PrintCorpora
+    { flagAsForests :: Bool
+    , argCorpora :: [FilePath]
+    }
+  | CBSM
+    { flagAsForests :: Bool
+    , flagDefoliate :: Bool
+    , flagGrammar :: FilePath
+    , argIterations :: Int
+    , argCorpora :: [FilePath]
     }
   | Parse
-    { argCount :: Int
-    , argGrammar :: FilePath
+    { argGrammar :: FilePath
+    , argCount :: Int
     }
   | Bests
-    { argCount :: Int
-    , argGrammar :: FilePath
+    { argGrammar :: FilePath
+    , argCount :: Int
     }
-  deriving (Data, Show, Typeable)
+  deriving Show
 
-argModes :: [Args]
-argModes
-  = [ CBSM
-      { argIterations = 0 &= argPos 0 &= typ "ITERATIONS"
-      , argCorpus = def &= argPos 1 &= typ "TREEBANK"
-      , argDefoliate = False &= explicit &= name "defoliate"
-          &= help "remove leafs from treebank trees, e.g. to just deal \
-                  \with preterminals"
-      , argGrammar = def &= explicit &= name "output" &= typ "GRAMMAR-FILE"
-          &= help "write binary representation of grammar to this file \
-                  \instead of pretty printing to stdout"
-      } &= details
-      [ "Read-off a grammar from TREEBANK and generalize it."
+argMode :: Mode Args
+argMode
+  = modes' "Main" (Help HelpFormatAll defaultWrap) "Count-Based State Merging"
+  [ flagHelpFormat (\ h t _ -> Help h t) ]
+  [ mode'
+      "print-corpora"
+      (PrintCorpora False [])
+      "Print trees from TREEBANKs. Can be used to check for parsing \
+      \errors. Every TREEBANK can be a file or a directory. Directories \
+      \are traversed recursively. If no TREEBANK is given, the trees are \
+      \read from standard input."
+      [flagNoneAsForests]
+      []
+      (Just flagArgCorpora)
+  , mode'
+      "cbsm"
+      (CBSM False False "" undefined [])
+      "Read-off a grammar from TREEBANKs and generalize it. See \
+      \printcorpora for further information about the TREEBANK arguments."
+      [ flagNoneAsForests
+      , flagNone ["defoliate"] (\ x -> x{flagDefoliate = True})
+          "remove leaves from trees in TREEBANKs"
+      , flagReq ["output"] (\ a x -> Right x{flagGrammar = a}) "FILE"
+          "write result to FILE instead of stdout"
       ]
-    , Parse
-      { argCount = 1 &= argPos 2 &= typ "COUNT"
-      , argGrammar = def &= argPos 3 &= typ "GRAMMAR-FILE"
-      } &= details
-      [ "Parse newline-separated sentences from standard input."
+      [ (flagArg (readUpdate $ \ a x -> x{argIterations = a}) "ITERATIONS")
+          {argRequire = True}
       ]
-    , Bests
-      { argCount = 1 &= argPos 4 &= typ "COUNT"
-      , argGrammar = def &= argPos 5 &= typ "GRAMMAR-FILE"
-      } &= details
-      [ "View best trees of a grammar."
+      (Just flagArgCorpora)
+  , mode'
+      "parse"
+      (Parse "" 1)
+      "Parse newline-separated sentences from standard input."
+      []
+      [ flagArgGrammar{argRequire = True}
+      , flagArgCount
       ]
-    ]
-  &= helpArg [name "h"]
-  &= versionArg [ignore]
-  &= program "command"
-  &= summary "Count-Based State Merging"
+      Nothing
+  , mode'
+      "bests"
+      (Bests "" 1)
+      "View best trees of a grammar."
+      []
+      [ flagArgGrammar{argRequire = True}
+      , flagArgCount
+      ]
+      Nothing
+  ]
+  where
+    flagNoneAsForests
+      = flagNone ["as-forests"] (\ x -> x{flagAsForests = True})
+          "the TREEBANKs contain forests instead of trees"
+    flagArgCorpora
+      = flagArg (\ a x -> Right x{argCorpora = argCorpora x ++ [a]}) "TREEBANK"
+    flagArgGrammar
+      = flagArg (\ a x -> Right x{argGrammar = a}) "GRAMMAR-FILE"
+    flagArgCount
+      = flagArg (readUpdate $ \ a x -> x{argCount = a}) "COUNT"
+
+
+readUpdate :: Read r => (r -> a -> a) -> Update a
+readUpdate update a x = fmap (\ r -> update r x) (readEither "no parse" a)
+
+
+readEither :: Read r => a -> String -> Either a r
+readEither msg cs = case reads cs of
+  (x, "") : _ -> Right x
+  _           -> Left msg
+
+mode' :: Name -> a -> Help -> [Flag a] -> [Arg a] -> Maybe (Arg a) -> Mode a
+mode' name value help flags args argSweeper
+  = (modeEmpty value)
+  { modeNames = [name]
+  , modeHelp = help
+  , modeArgs = (args, argSweeper)
+  , modeGroupFlags = toGroup flags
+  }
+
+
+modes' :: Name -> a -> Help -> [Flag a] -> [Mode a] -> Mode a
+modes' name value help flags xs = (modeEmpty value)
+  { modeNames      = [name]
+  , modeHelp       = help
+  , modeGroupFlags = toGroup flags
+  , modeGroupModes = toGroup xs
+  }
 
 
 type BinaryCRTG = CRTG Int String
@@ -96,22 +165,31 @@ type BinaryCRTG = CRTG Int String
 
 main :: IO ()
 main = do
-  arguments <- cmdArgs (modes argModes)
-  print arguments
+  arguments <- processArgs argMode
   case arguments of
 
+    Help helpFormat textFormat
+      -> putStr
+       $ showText textFormat
+       $ helpText [] helpFormat argMode
+
+    PrintCorpora{..}
+       -> putStr
+        . unlines
+        . concatMap (\ (i, t) -> [show i ++ ":", drawTreeColored t])
+        . zip [1 :: Int ..]
+      =<< readCorpora flagAsForests argCorpora
+
     CBSM{..}
-       -> ( if null argGrammar
+       -> ( if null flagGrammar
             then print
-            else B.encodeFile argGrammar :: BinaryCRTG -> IO ()
+            else B.encodeFile flagGrammar :: BinaryCRTG -> IO ()
           )
-        . (!! argIterations)
+      =<< progress (\ i -> "Iteration " ++ show i) argIterations
         . cbsm
         . forestToGrammar
-        . map (if argDefoliate then T.defoliate else id)
-        . map Penn.toTree
-        . either (error . show) id
-      =<< parseFromFile Penn.p_penn argCorpus
+        . map (if flagDefoliate then T.defoliate else id)
+      =<< readCorpora flagAsForests argCorpora
 
     Parse{..} -> do
       (hg, inis) <- toHypergraph <$> (B.decodeFile argGrammar :: IO BinaryCRTG)
@@ -121,7 +199,7 @@ main = do
       let feature = F.Feature (\ _ (i, _) xs -> i * product xs) V.singleton
       sents <- map words . lines <$> getContents
       forM_ sents $ \ sent -> do
-        let (hg', weights') = earley' (asBackwardStar hg) comp (WSA.fromList 1 sent) (M.keys inis)
+        let (hg', _) = earley' (asBackwardStar hg) comp (WSA.fromList 1 sent) (M.keys inis)
         let inis' = M.mapKeys (\ k -> (0, k, length sent)) inis
         printWeightedDerivations
           $ take argCount
@@ -135,17 +213,49 @@ main = do
         $ bestsIni (asBackwardStar hg) feature (V.singleton 1) inis
 
   where
+    readCorpora :: Bool -> [FilePath] -> IO (Forest String)
+    readCorpora asForests corpora
+        = (if asForests then concatMap SExp.toForest else map SExp.toTree)
+      <$> if null corpora
+            then SExp.parse SExp.pSExpressions "stdin"
+             <$> getContents
+            else concat
+             <$> (   SExp.parseFromFiles SExp.pSExpressions
+                 =<< getContentsRecursive corpora
+                 )
+
     printWeightedDerivations xs =
       forM_ (zip [1 :: Int ..] xs) $ \ (i, (w, t)) -> do
         let t' = fmap H.label t
         putStrLn $ show i ++ ": " ++ show w
         putStrLn $ unwords $ yield t'
-        putStrLn
-          $ drawTree' (drawstyleCompact2 0 "")
-          $ mapLeafs (\ cs -> "\ESC[93m" ++ cs ++ "\ESC[m") t'  -- colorize
+        putStrLn $ drawTreeColored t'
 
 
--- bests :: (Ord v, Eq l) => CRTG v l -> [(Double, H.Derivation v l Double)]
+drawTreeColored :: Tree String -> String
+drawTreeColored
+  = drawTree' (drawstyleCompact2 0 "")
+  . mapLeafs (\ cs -> "\ESC[93m" ++ cs ++ "\ESC[m")
+
+
+progress :: (Int -> String) -> Int -> [a] -> IO a
+progress msg n xs0 = go xs0 0
+  where
+    go [] _ = errorHere "progress" "list too short"
+    go (x : xs) i = do
+      putStrLn $ msg i
+      x `seq` if i >= n
+        then return x
+        else go xs $! i + 1
+
+
+bestsIni
+  :: (H.Hypergraph h, Ord v, Eq l)
+  => h v l i
+  -> F.Feature l i x
+  -> V.Vector Double
+  -> M.Map v Double
+  -> [(Double, H.Derivation v l i)]
 bestsIni hg feat wV inis
   = mergesBy (comparing (Down . fst))
   $ M.elems
@@ -166,6 +276,25 @@ bestsIni hg feat wV inis
           _  ->  x : mergeBy cmp xs' ys
     mergeBy _ [] ys = ys
     mergeBy _ xs [] = xs
+
+
+getContentsRecursive :: [FilePath] -> IO [FilePath]
+getContentsRecursive paths
+  = fmap concat
+  $ forM paths $ \ path ->
+      ifM (doesDirectoryExist path)
+        ( getDirectoryContents path
+          >>= getContentsRecursive
+            . map (path </>)
+            . filter (`notElem` [".", ".."])  -- TODO: this seems like a hack
+        )
+        (return [path])
+
+
+ifM :: Monad m => m Bool -> m b -> m b -> m b
+ifM predicateM thn els = do
+  b <- predicateM
+  if b then thn else els
 
 
 test3 :: (Show l, Ord l) => Int -> [Tree l] -> IO ()
