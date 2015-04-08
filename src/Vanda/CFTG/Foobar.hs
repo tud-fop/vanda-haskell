220e10f377cf34c8d4b0bc00ede52998eaf6d847
Toni Dietze <toni.dietze@tu-dresden.de>
2015-04-08 14:38:39 +0200

    Vanda.CFTG.Foobar.blockdiagram{ASCII,SVG}: new functions

--- src/Vanda/CFTG/Foobar.hs
+++ src/Vanda/CFTG/Foobar.hs
@@ -39,6 +39,35 @@ alwaysSameRule
     step = concatMap $ \ (x, y) -> [(succ x, 0), (0, succ y)]
 
 
+
+-- try: mapM_ (putStrLn . blockdiagramASCII) $ take 6 alwaysSameRule
+blockdiagramASCII :: [Int] -> String
+blockdiagramASCII xs
+  = unlines
+  $ (concat (map show xs) :)
+  $ map line [1 .. maximum xs]
+  where line n = map (\ x -> if x >= n then '█' else ' ') xs
+
+
+-- try: writeFile "test.svg" $ blockdiagramSVG $ alwaysSameRule !! 13
+blockdiagramSVG :: [Int] -> String
+blockdiagramSVG xs
+  = unlines
+      [ svgHead (show (2 * length xs)) (show (yScale * maximum xs))
+      , unlines $ map line $ zip [0 ..] xs
+      , svgTail
+      ]
+  where
+    yScale = 100
+    line :: (Int, Int) -> String
+    line (x, y)
+      = "<line x1=\"" ++ show (2 * x + 1)
+      ++ "\" y1=\"" ++ "0"
+      ++ "\" x2=\"" ++ show (2 * x + 1)
+      ++ "\" y2=\"" ++ show (yScale * y)
+      ++ "\" stroke=\"black\" stroke-width=\"2\"/>"
+
+
 printLanguage :: Show a => [[a]] -> IO ()
 printLanguage = mapM_ printWord
 
@@ -76,3 +105,19 @@ pairUp :: [a] -> [(a, a)]
 pairUp (x : y : zs) = (x, y) : pairUp zs
 pairUp []           = []
 pairUp _            = errorHere "pairUp" "uneven list length"
+
+
+-- https://de.wikipedia.org/wiki/Scalable_Vector_Graphics#Dokument
+svgHead :: String -> String -> String
+svgHead w h = unlines
+  [ "<?xml version=\"1.0\" encoding=\"UTF-8\"?>"
+  , "<!DOCTYPE svg PUBLIC \"-//W3C//DTD SVG 1.1//EN\" \"http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd\">"
+  , ""
+  , "<svg xmlns=\"http://www.w3.org/2000/svg\""
+  , "     xmlns:xlink=\"http://www.w3.org/1999/xlink\" xmlns:ev=\"http://www.w3.org/2001/xml-events\""
+  , "     version=\"1.1\" baseProfile=\"full\""
+  , "     width=\"" ++ w  ++ "\" height=\"" ++ h ++ "\">"
+  ]
+
+svgTail :: String
+svgTail = "</svg>"
