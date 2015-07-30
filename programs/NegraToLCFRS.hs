87cc7b8cda12f5de803a16252c301ac253a73f0e
Tobias Denkinger <tobias.denkinger@tu-dresden.de>
2015-07-30 12:16:03 +0200

    programs/NegraToLCFRS.hs: now reads from stdin

--- programs/NegraToLCFRS.hs
+++ programs/NegraToLCFRS.hs
@@ -17,23 +17,23 @@ import qualified Data.ByteString.Lazy as BS
 import qualified Data.Map.Strict as M
 import qualified Data.Text.Lazy.IO as TIO
 import           System.Environment (getArgs, getProgName)
+import           System.IO (hPutStrLn, stderr)
 
-import Vanda.Corpus.Negra.Text
+import Vanda.Corpus.Negra.Text (parseNegra)
 import Vanda.Grammar.XRS.LCFRS (niceStatictics)
-import Vanda.Grammar.XRS.LCFRS.Extraction
+import Vanda.Grammar.XRS.LCFRS.Extraction (extractPLCFRSFromNegra)
 
 
--- | Extracting a PLCFRS
 main :: IO ()
 main = do
   progName <- getProgName
   args <- getArgs
   case args of
-    ["-n", negrafile, "-o", outfile] -> do
-      plcfrs <- fmap (extractPLCFRSFromNegra . parseNegra)
-              $ TIO.readFile negrafile
-      BS.writeFile outfile (compress $ B.encode plcfrs)
-      putStrLn $ "Extracted PLCFRS:" ++ niceStatictics plcfrs
-    _ -> putStrLn $ "Usage: " ++ progName ++ " -n negrafile -o outfile"
-                    ++ "\nnegrafile is an export format corpus file"
-                    ++ ", outfile will be a gzipped plcfrs file"
+    [outfile] -> do
+      corpus <- TIO.getContents
+      let plcfrs = extractPLCFRSFromNegra $ parseNegra corpus
+      BS.writeFile outfile . compress $ B.encode plcfrs
+      hPutStrLn stderr $ "Extracted PLCFRS:" ++ niceStatictics plcfrs
+    _ -> putStrLn $ "Usage: " ++ progName ++ " <outfile>"
+                    ++ "\n  a NeGra corpus file is read from stdin"
+                    ++ ", outfile will be a gzipped PLCFRS file"
