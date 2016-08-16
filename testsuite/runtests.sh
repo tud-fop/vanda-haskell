adc1428b4624d663cabc2c8830347fba87ab6223
Toni Dietze <toni.dietze@tu-dresden.de>
2016-08-16 10:28:19 +0200

    testsuite/runtests.sh: use cabal’s sandbox; renamed build to build.nobackup

--- testsuite/runtests.sh
+++ testsuite/runtests.sh
@@ -3,15 +3,18 @@
 set -Ceu
 
 cd "${0%/*}"            ; declare -r scriptDir="${PWD}"
+cd "${scriptDir}/.."    ; declare -r baseDir="${PWD}"
 cd "${scriptDir}/../src"; declare -r srcDir="${PWD}"
 cd "${scriptDir}/tests" ; declare -r testsDir="${PWD}"
 
-declare -r tempDir="${scriptDir}/build"
+declare -r tempDir="${scriptDir}/build.nobackup"
 mkdir -p "${tempDir}"
 # declare -r tempDir="$(mktemp --directory --suffix=".${0##*/}")"
 # trap 'rm -rf "${tempDir}"; trap - ERR EXIT' ERR EXIT
 
 function runTest() {
+	cd "${baseDir}"
+	export GHC_PACKAGE_PATH="$(cabal exec -- bash -c 'echo -E "${GHC_PACKAGE_PATH}"')"
 	cd "${tempDir}"
 	rm -f *.tix
 	# ↱ workaround “Warning: Couldn't figure out linker information!”
@@ -33,7 +36,7 @@ function runTest() {
 	echo
 	hpc report tests
 	echo
-	hpc markup --hpcdir="../build/.hpc" --srcdir="${srcDir}" --srcdir="${testsDir}" tests >/dev/null  \
+	hpc markup --hpcdir="../build.nobackup/.hpc" --srcdir="${srcDir}" --srcdir="${testsDir}" tests >/dev/null  \
 		&& echo 'You can find detailed html pages about code coverage in the following directory:'  \
 		&& echo "${tempDir}"
 }
