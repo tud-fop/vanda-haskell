f365bced1761cf5dcb57aaca5674518c5d5b8a0d
nwuensche <Niklas.Wuensche@mailbox.tu-dresden.de>
2018-05-15 14:02:35 +0200

    Add a lot of stuff from active parser, write comments to it

--- library/Vanda/Grammar/XRS/LCFRS/IncrementalParser.hs
+++ library/Vanda/Grammar/XRS/LCFRS/IncrementalParser.hs
@@ -53,6 +53,10 @@ type Container nt t wt = ( C.Chart nt t wt -- Passive Items
                          , MMap.MultiMap nt (Item nt t wt) --Map NT onto List of Active Items, that need NT in the next Step
                          , Set.HashSet nt -- All NTs, which are not init. right now
                          )
+update :: Container nt t wt -> Item nt t wt -> (Container nt t wt, Bool)
+update (p, a, n) item@(Active (Rule ((_, as),_)) _ _ ((Var i _:_):_) _ _)
+ = ((p, MMap.insert (as !! i) item a, (as !! i) `Set.delete` n), True)
+update (p, a, n) _ = ((p, a, n), True)
 
 
 -- From active Parser
@@ -61,7 +65,78 @@ type Container nt t wt = ( C.Chart nt t wt -- Passive Items
       -> Int -- Beam Width
       -> Int -- Max. Amount of Parse Trees
       -> [t] -- Word
-      -> MMap.MultiMap nt (Item nt t wt)-} -- Map of Active Items for Testing Rules, -> [Tree (Rule nt t)] Later
--- parse g bw tops w = parse' (prepare g w) bw tops w
+      -> [Tree (Rule nt t)]
+parse g bw tops w = parse' (prepare g w) bw tops w
+
+parse' :: forall nt t wt.(Hashable nt, Hashable t, Eq t, Ord wt, Weight wt, Ord nt, Converging wt) 
+       => (MMap.MultiMap nt (Rule nt t, wt), Map.HashMap nt (wt,wt), [nt]) -- prepare Result (RuleMap NT-Rules, IO-Weights NTs, Reachable Items
+       -> Int -- Beam Width
+       -> Int -- Max. Amount of Parse Trees
+       -> [t] -- Word
+       -> [Tree (Rule nt t)]
+parse' (rmap, iow, s') bw tops w
+  = C.parseTrees tops s' 
+    (singleton $ entire w) -- Goal Item
+  $ (\ (e, _, _) -> e) -- parse Trees just needs passive Items from Container
+  $ C.chartify (C.empty, MMap.empty, nset) update rules bw tops
+    where
+      nset = Set.fromList $ filter (not . (`elem` s')) $ Map.keys rmap
+      
+      rules = initialPrediction w (s' >>= (`MMap.lookup` rmap)) iow
+-}
+
+convert :: (Item nt t wt, wt) -> Maybe (Item nt t wt, wt)
+convert (Active r w rs [] completions inside, heuristic)
+  = case fromList $ reverse rs of
+         Nothing -> Nothing
+         Just rv -> let rvs = IMap.elems completions
+                        (Rule ((a, _), _)) = r
+                    in Just (Passive a rv (C.Backtrace r w rvs) inside, heuristic)
+convert i@(Active _ _ rs _ _ _, _)
+  | isNonOverlapping rs = Just i
+  | otherwise = Nothing
+convert _ = Nothing
+
+-- Wenn Item direkt passive ist, dann wird letztes Schritt über aktive Item nicht mehr in Map aufgenommen, vor passive Item weggelassen, da ich das nicht brauche. Falls nicht Konvertiert werden kann, dann einfach dieses Item zurückgeben
+predictionRule :: forall nt t wt. (Weight wt, Eq nt, Hashable nt, Eq t) 
+               => [t] -- Word
+               -> MMap.MultiMap nt (Rule nt t, wt) -- Rules from prepare: NT->Rules Map
+               -> Map.HashMap nt (wt, wt) -- IO-Weigths from prepare
+               -> C.ChartRule (Item nt t wt) wt (Container nt t wt)
+predicitionRule word rs ios = Right app
+    where
+        app :: Item nt t wt -- Trigger Item
+            -> Container nt t wt
+            -> [(Item nt t wt, wt)] -- New Items from using Trigger with Container by using prediction Rule
+        app (Active (Rule ((_, as), _)) w _ ((Var i _:_):_) _ _) (_, _, inits) --Var i -> Betrachte i-te Eingangsvektor Xi
+        = catMaybes [ (Active r' w rho'' f'' IMap.empty inside, inside <.> outside) 
+        | let a = as !! i -- Nimm den i-ten Einfangsvektor Xi
+        , (r'@(Rule ((a', as'), f')), w') <- MMap.lookup a rs -- Baue für jede Regel, die  Xi -> ... ist, neues Item auf
+        , (rho'', f'') <- com
+        ] 
+
+-- Nimmt gefundene Ranges für ein NT + Aktuelle Ranges + Regel -> Spuckt neue Ranges und Funktionen aus, in denen alles, was möglich ist, eingesetzt wurde
+completeKnownTokens :: (Eq t)
+                    => [t]  -- Word
+                    -> IMap.IntMap Rangevector  -- Map von Ranges von NTs, welche ich schon ersetzt habe A1->...
+                    -> [Range] -- Ranges, welche ich hinzufügen will?
+                    -> Function t -- Ersetzungfunktion
+                    -> [([Range], Function t)] -- Neue Range und Funktion
+completeKnownTokens _ _ rs [[]] = [(rs, [])]
+completeKnownTokens w m rs ([]:fs) = completeKnownTokens w m (Epsilon:rs) fs
+completeKnownTokens w m (r:rs) ((T t:fs):fss) 
+  = [ (r':rs, fs:fss)
+    | r' <- mapMaybe (safeConc r) $ singletons t w
+    ] >>= uncurry (completeKnownTokens w m)
+completeKnownTokens w m (r:rs) ((Var i j:fs):fss) 
+  = case i `IMap.lookup` m of
+         Just rv -> case safeConc r (rv ! j) of
+                         Just r' -> completeKnownTokens w m (r':rs) (fs:fss)
+                         Nothing -> []
+         Nothing -> [(r:rs, (Var i j:fs):fss)]
+completeKnownTokens _ _ _ _ = []
 
 
+
+-- TODO convert active Item noch überall rein, sobald ich weiß, dass mein aktive Item richtig ist und ich daraus passive Item ableiten kann
+--
