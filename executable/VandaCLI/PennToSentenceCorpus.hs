5120844ded2788daac0d3fc2ceed97908424774c
Thomas Ruprecht <thomas.ruprecht@tu-dresden.de>
2018-07-30 10:48:58 +0200

    remove old files, programs → executable

---
+++ executable/VandaCLI/PennToSentenceCorpus.hs
@@ -0,0 +1,23 @@
+module VandaCLI.PennToSentenceCorpus where
+
+import Vanda.Corpus.Penn.Text
+import qualified Data.Text.Lazy.IO as TIO
+import Data.Tree
+
+import System.Console.CmdArgs.Explicit
+import System.Console.CmdArgs.Explicit.Misc
+
+data Args = Help String | Args String deriving Show
+
+cmdArgs :: Mode Args
+cmdArgs = (modeEmpty $ Args "-"){ modeNames = ["penn-to-sentence-corpus"]
+                                , modeHelp = "Reads of the yield of trees in a PennTreeCorpus."
+                                , modeArgs = ([flagArg (\ s _ -> Right $ Args s) "FILE"], Nothing)
+                                , modeGroupFlags = toGroup [flagNone ["help"] (\ _ -> Help $ defaultHelp cmdArgs) "Prints the help message."]
+                                }
+
+mainArgs :: Args -> IO ()
+mainArgs (Help cs) = putStr cs
+mainArgs (Args filename)
+  = do cTrees <- if filename == "-" then TIO.getContents else TIO.readFile filename
+       TIO.putStr . yield $ (parsePenn cTrees :: [Tree String])
